94425799240d2add7663eb3a8dac5378653ba4f8
Gellert <gellerrt@gmail.com>
2020-10-13 21:17:53 +0300

    init

---
+++ src/main/java/ru/botans/games/parser/gameparser/GameparserApplication.java
@@ -0,0 +1,13 @@
+package ru.botans.games.parser.gameparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameparserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameparserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/botans/games/parser/gameparser/GameparserApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.botans.games.parser.gameparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameparserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
